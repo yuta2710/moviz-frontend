93fc61f26681783c0f4ab75975c748f004a4a339
vqgquan <s3927120@rmit.edu.vn>
2023-12-29 18:06:43 +0700

    Update page.tsx

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -326,7 +326,7 @@ export default function Page(): ReactElement {
         ))
     }
   </ul>
-  
+
   return (
     <div className="relative top-0">
       <div className="background-custom-body" style={{ height: "fit-content" }}>
@@ -342,11 +342,9 @@ export default function Page(): ReactElement {
         <div className="flex flex-row">
           <div className="blob relative"></div>
           {/* <div className="blob-linear-green-blue relative"></div> */}
-          
         </div>
 
-
-      { /** Reviews List */}
+        { /** Reviews List */}
       {reviews.length > 0 && (
         <div className="flex flex-col justify-center items-center relative md:mt-12">
           <h1 className="text-white text-lg md:text-2xl font-semibold relative text-left">Popular Reviews On This Week</h1>
@@ -356,43 +354,11 @@ export default function Page(): ReactElement {
         </div>
       )}
 
-      { /** Movie List */}
-      {movies.length > 0 && (
-        <div className="flex flex-col justify-center relative md:mt-16">
-          <h1 className="text-white text-2xl font-semibold relative text-center">Popular Movies On This Week</h1>
-          <div className="flex flex-row justify-center items-center relative md:mt-12 hidden md:flex">
-            <h1 className="text-white text-[1.2rem] font-semibold relative text-left">View By</h1>
-            <select className="md:ml-6 text-gray-900 text-sm relative rounded-2xl block md:w-[120px] md:p-1.5 apple-linear-glass dark:placeholder-gray-400 dark:text-white" value={year} onChange={handleOnChangeYear}>
-              {yearOptions.map((option) => (
-                <option value={option.value} className="text-center">{option.label}</option>
-              ))}
-            </select>
-
-            <select className="md:ml-6 text-gray-900 text-sm relative rounded-2xl block md:w-[120px] md:p-1.5 apple-linear-glass dark:placeholder-gray-400 dark:text-white" onChange={handleOnChangeRating}>
-              <option value="" disabled selected className="text-center">Rating</option>
-              {ratingOptions.map((option) => (
-                <option value={option.value} className="text-center">{option.label}</option>
-              ))}
-            </select>
-            <select className="md:ml-6 text-gray-900 text-sm relative rounded-2xl block md:w-[120px] md:p-1.5 apple-linear-glass dark:placeholder-gray-400 dark:text-white" value={popular} onChange={handleOnChangePopular}>
-              {popularOptions.map((option) => (
-                <option value={option.value} className="text-center">{option.label}</option>
-              ))}
-            </select>
-            <select className="md:ml-6 text-gray-900 text-sm relative rounded-2xl block md:w-[120px] md:p-1.5 apple-linear-glass dark:placeholder-gray-400 dark:text-white" onChange={handleOnChangeGenre}>
-              <option value="" disabled selected className="text-center">Genre</option>
-              {genres.map((genre) => (
-                <option value={genre.id} className="text-center">{genre.name}</option>
-              ))}
-            </select>
-          </div>
-        )}
-
         { /** Movie List */}
         {movies.length > 0 && (
           <div className="flex flex-col justify-center relative md:mt-16">
             <h1 className="text-white text-2xl font-semibold relative text-center">Popular Movies On This Week</h1>
-            <div className="flex flex-row justify-center items-center relative md:mt-12">
+            <div className="flex flex-row justify-center items-center relative md:mt-12 hidden md:flex">
               <h1 className="text-white text-[1.2rem] font-semibold relative text-left">View By</h1>
               <select className="md:ml-6 text-gray-900 text-sm relative rounded-2xl block md:w-[120px] md:p-1.5 apple-linear-glass dark:placeholder-gray-400 dark:text-white" value={year} onChange={handleOnChangeYear}>
                 {yearOptions.map((option) => (
@@ -426,31 +392,20 @@ export default function Page(): ReactElement {
               {/* <div className="blob relative"></div> */}
               <div className="blob-linear-yellow-blue relative"></div>
             </div>
-            <ul className="grid grid-cols-3 md:mx-auto relative gap-4 justify-center items-center md:mt-8">
-              {[...movies]
-                .slice(0, 6)
-                .map((movie) => (
-                  <li className="hover:scale-105 duration-500 rotate_3d m-0 rounded-2xl">
-                    <Link href={`/movies/${movie.id}`} className="block max-w-sm p-6 rounded-lg shadow movie-obj">
-                      <Image src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} width={200} height={0} alt="" className="md:mx-auto object-cover rounded-sm"></Image>
-                    </Link>
-                  </li>
-                ))}
-            </ul>
+              <ul className="grid grid-cols-2 md:grid-cols-3 md:mx-auto relative gap-4 justify-center items-center md:mt-8">
+                {[...movies]
+                  .slice(0, 6)
+                  .map((movie) => (
+                    <li className="col-span-1 hover:scale-105 duration-500 rotate_3d m-0 rounded-2xl">
+                      <Link href={`/movies/${movie.id}`} className="block max-w-sm p-6 rounded-lg shadow movie-obj">
+                        <Image src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} width={200} height={0} alt="" className="md:mx-auto object-cover rounded-sm"></Image>
+                      </Link>
+                    </li>
+                  ))}
+              </ul>
           </div>
-          <ul className="grid grid-cols-2 md:grid-cols-3 md:mx-auto relative gap-4 justify-center items-center md:mt-8">
-            {[...movies]
-              .slice(0, 6)
-              .map((movie) => (
-                <li className="col-span-1 hover:scale-105 duration-500 rotate_3d m-0 rounded-2xl">
-                  <Link href={`/movies/${movie.id}`} className="block max-w-sm p-6 rounded-lg shadow movie-obj">
-                    <Image src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} width={200} height={0} alt="" className="md:mx-auto object-cover rounded-sm"></Image>
-                  </Link>
-                </li>
-              ))}
-          </ul>
-        </div>
-      )}
+        )}
+      </div>
     </div>
   )
 
