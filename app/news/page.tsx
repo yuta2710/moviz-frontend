5f8f6a13508562035b5998863b6570dc8d7f6452
Nguyen Phuc Loi <digivantrix0802.ent@gmail.com>
2024-01-07 02:35:20 +0700

    Complete UI of movies and movie ID in frontend

--- app/news/page.tsx
+++ app/news/page.tsx
@@ -57,7 +57,7 @@ export default function Page() {
     router.push(`/news?page=${pageNumber}`);
   };
 
-  return (<>
+  return <>
     <div className="container my-12 mx-auto px-4 md:px-12 relative">
       <div className="flex flex-row">
         {/* <div className="blob relative"></div> */}
