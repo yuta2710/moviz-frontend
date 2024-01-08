2adcb1a2f179f9f0d82a347297f7a3f93d44be4a
Nguyen Phuc Loi <digivantrix0802.ent@gmail.com>
2024-01-09 00:32:41 +0700

    Commit something FE

--- utils/clients.utils.ts
+++ utils/clients.utils.ts
@@ -156,33 +156,6 @@ export async function getRelatedMovies(id: string) {
   }
 }
 
-export async function getRecommendations(id: string) {
-  const url = `https://api.themoviedb.org/3/movie/${id}/recommendations`;
-  const options = {
-    method: "GET",
-    headers: {
-      accept: "application/json",
-      Authorization:
-        "Bearer eyJhbGciOiJIUzI1NiJ9.eyJhdWQiOiJkOTVhODkyNmVmNjJmYzJhNWMzY2EyMmI4YTk1YjkxYiIsInN1YiI6IjY0YjBlOTRjNGU0ZGZmMDBlMmY4OWM4OCIsInNjb3BlcyI6WyJhcGlfcmVhZCJdLCJ2ZXJzaW9uIjoxfQ.uNP0Bt35sJlucLBeFZUCRvUv_1Si-S9CxsN_8cLhrBY",
-    },
-  };
-
-  try {
-    const response = await fetch(url, options);
-    if (!response.ok) {
-      throw new Error(`HTTP error! Status: ${response.status}`);
-    }
-
-    const json = await response.json();
-
-    console.log("Json = ", json);
-    return json;
-  } catch (error) {
-    console.error("Error:", error);
-    throw error; // Re-throw the error for the caller to handle
-  }
-}
-
 export async function getCasts(id: string) {
   const url = `https://api.themoviedb.org/3/movie/${id}/credits`;
   const ENDPOINT = `http://localhost:8080/api/v1/movies/${id}/casts`;
