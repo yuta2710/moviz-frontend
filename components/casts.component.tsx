069bf4d0e2e5fe9ceb0d91974a669961175f6641
vinhhungnh <89514560+vinhhungnh@users.noreply.github.com>
2024-01-12 17:25:38 +0700

    cast modal update

--- components/casts.component.tsx
+++ components/casts.component.tsx
@@ -98,7 +98,6 @@ const Casts = ({ id }: { id: string }) => {
     justifyContent: "center",
   };
 
-
   return (
     <div className='flex flex-col justify-center items-center'>
       <h1 className='text-left text-2xl font-semibold text-white md:ml-16'>Casts</h1>
@@ -110,8 +109,8 @@ const Casts = ({ id }: { id: string }) => {
           //   <h2>{cast.character}</h2>
           // </div>
           cast.profile_path !== null && (
-            <div className='flex flex-col w-28 h-max cast-key cursor-pointer' onClick={() => handleOpenCastInfo(cast.id)}>
-              <Image src={`https://image.tmdb.org/t/p/original/${cast.profile_path}`} alt='cast-img' width={100} height={150}></Image>
+            <div className='flex flex-col w-28 h-max cast-key'>
+              <Image src={`https://image.tmdb.org/t/p/original/${cast.profile_path}`} alt='cast-img' width={100} height={150} onClick={() => handleOpenCastInfo(cast.id)}></Image>
               <h2 className='text-sm font-bold text-white text-left md:mt-2'>{cast.name}</h2>
               <h2 className='text-[0.8rem] font-regular text-gray-400 md:mt-1'>{cast.character}</h2>
 
